ff5a1fb2629d2be95c922329c07b218beace53ca
aloongside <113152170+aloongside@users.noreply.github.com>
2022-10-19 17:21:58 -0400

    New Candlestick Patterns with only 2 functions

--- Candlestick.py
+++ Candlestick.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 #Obtained (1 = oldest, 3 = newest)
-high = 116.93
-open = 116.39
-close = 116.83
-low = 116.29
-high2 = 116.95
-open2 = 116.83
-close2 = 116.84
-low2 = 116.48
-high3 = 116.74
-open3 = 116.71
-close3 = 115.71
-low3 = 115.19
+high = 113.97
+open = 113.86
+close = 113.61
+low = 113.58
+high2 = 113.64
+open2 = 113.60
+close2 = 113.58
+low2 = 113.27
+high3 = 114.14
+open3 = 113.58
+close3 = 114.11
+low3 = 113.57
 
 
 # Derived
@@ -45,63 +45,66 @@ class Candlestick():
 
 
 # Recognition for Single Candlestick Patterns only
-def recognSingleCSPattern(new):
-    # Could change to new.high == new.top for a more strict pattern
-    if (new.bottom_wick/new.top_wick > 3) and (new.bullish == True) and (new.bottom_wick/new.body > 2): # Hammer, gain
+
+def buyCSPattern(prev2,prev1,new):
+    if (new.bullish == True) and (prev1.bullish == True) and (prev2.bullish == True):
+        print("3 White Soldiers!") # gain
+        return True
+    elif (new.bullish == True) and (prev1.bullish == False) and (prev2.bullish == False) and (prev2.body_ratio > .65) and (new.body_ratio > .75) and (prev2.bottom > prev1.mid) and (new.bottom > prev1.mid):
+        print("Morning Star!") # gain
+        return True
+    elif (prev1.bullish == False) and (new.bullish == True) and (prev1.bottom > new.bottom) and (prev1.top < new.top) and (new.body/prev1.body > 1):
+        print("Bullish Engulfing!") # gain
+        return True
+    elif (prev1.bullish == False) and (new.bullish == True) and (new.mid < prev1.mid < new.top) and (prev1.body_ratio > .75) and (new.body_ratio > .75):
+        print("Piercing Line!") # gain
+        return True
+    elif (new.bottom_wick/new.top_wick > 3) and (new.bullish == True) and (new.bottom_wick/new.body > 2): # Hammer, gain
         print("Hammer!")
-    elif (new.bottom_wick/new.top_wick > 3) and (new.bullish == False) and (new.bottom_wick/new.body > 2): # Hangman, lose
-        print("Hangman!") #Confirmed to work
+        return True
     elif (new.top_wick/new.bottom_wick > 3) and (new.bullish == True) and (new.top_wick/new.body > 2): # Inverse Hammer, gain
         print("Inverse Hammer!")
+        return True
+    else:
+        print("No Buy Candlestick Patterns")
+        return False
+
+def sellCSPattern(prev2,prev1,new):
+    if (new.bullish == False) and (prev1.bullish == False) and (prev2.bullish == False):
+        print("3 Black Crows!") # lose
+        return True
+    elif(new.bullish == False) and (prev1.bullish == True) and (prev2.bullish == True) and (prev2.body_ratio > .65) and (new.body_ratio > .6) and (prev2.mid < prev1.bottom) and (new.top < prev1.bottom):
+        print("Evening Star!")
+        return True
+    elif (prev1.bullish == True) and (new.bullish == False) and (prev1.bottom > new.bottom) and (prev1.top < new.top) and (new.body/prev1.body > 1):
+        print("Bearish Engulfing!") # Confirmed to work, lose
+        return True
+    elif (prev1.bullish == True) and (new.bullish == False) and (new.bottom < prev1.mid < new.mid) and (prev1.body_ratio > .75) and (new.body_ratio > .75):
+        print("Dark Cloud Cover!") # lose
+        return True
+    elif (new.bottom_wick/new.top_wick > 3) and (new.bullish == False) and (new.bottom_wick/new.body > 2): # Hangman, lose
+        print("Hangman!") #Confirmed to work
+        return True
     elif (new.top_wick/new.bottom_wick > 3) and (new.bullish == False) and (new.top_wick/new.body > 2): # Shooting Star, lose
         print("Shooting Star!")
+        return True
     else:
-        print("No Single Candlestick Patterns")
+        print("No Sell Candlestick Patterns")
+        return False
 
-
-# Recognition for Double Candlestick Patterns only
-def recognDoubleCSPattern(prev,new):
-    if (prev.bullish == False) and (new.bullish == True) and (prev.bottom > new.bottom) and (prev.top < new.top) and (new.body/prev.body > 1):
-        print("Bullish Engulfing!") # gain
-    elif (prev.bullish == False) and (new.bullish == True) and (new.mid < prev.mid < new.top) and (prev.body_ratio > .75) and (new.body_ratio > .75):
-        print("Piercing Line!") # gain
-    elif (prev.bullish == True) and (new.bullish == False) and (prev.bottom > new.bottom) and (prev.top < new.top) and (new.body/prev.body > 1):
-        print("Bearish Engulfing!") # Confirmed to work, lose
-    elif (prev.bullish == True) and (new.bullish == False) and (new.bottom < prev.mid < new.mid) and (prev.body_ratio > .75) and (new.body_ratio > .75):
-        print("Dark Cloud Cover!") # lose
-    else:
-        print("No Double Candlestick Patterns")
-
-
-# Recognition for Triple Candlestick Patterns only
-def recognTripleCSPattern(prev2,prev1,new):
-    if (new.bullish == True) and (prev1.bullish == True) and (prev2.bullish == True):
-        # new.body_ratio > .75 (for new,prev1,prev2) and...
-        # prev2.mid < prev1.bottom and prev1.mid < new.bottom for a stronger pattern
-        print("3 White Soldiers!") # gain
-    elif (new.bullish == False) and (prev1.bullish == False) and (prev2.bullish == False):
-        print("3 Black Crows!") # lose
-    elif (new.bullish == True) and (prev1.bullish == False) and (prev2.bullish == False) and (prev2.body_ratio > .65) and (new.body_ratio > .75) and (prev2.bottom > prev1.mid) and (new.bottom > prev1.mid):
-        # Change last 2 conditons from mid to top to not allow bar overlap, more strict/traditional pattern
-        print("Morning Star!")
-    elif(new.bullish == False) and (prev1.bullish == True) and (prev2.bullish == True) and (prev2.body_ratio > .65) and (new.body_ratio > .6) and (prev2.mid < prev1.bottom) and (new.top < prev1.bottom):
-        # Same thing...can change mid at end to top to create a more strict pattern
-        print("Evening Star!")
-    else:
-        print("No Triple Candlestick Patterns")
 
 
 def main():
     new = Candlestick(high,open,close,low)
-    recognSingleCSPattern(new)
-
     prev = new
     new = Candlestick(high2,open2,close2,low2)
-    recognDoubleCSPattern(prev,new)
-
     prev2 = prev
-    prev = new
+    prev1 = new
     new = Candlestick(high3,open3,close3,low3)
-    recognTripleCSPattern(prev2,prev,new)
+
+    bool1 = buyCSPattern(prev2,prev1,new)
+    bool2 = sellCSPattern(prev2,prev1,new)
+
+
 
 main()
