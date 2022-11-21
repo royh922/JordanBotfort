3c0bd2f9932a584059e9730019a563baf1f97885
royh922 <hjuiteng@gmail.com>
2022-11-20 23:15:50 -0500

    refactor system architecture for multi-stock trading

---
+++ build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.JordanBotfort.bot","l":"Commands"},{"p":"me.JordanBotfort.bot","l":"Jordan"}];updateSearchResults();
---
+++ src/main/java/me/JordanBotfort/bot/Jordan.java
@@ -0,0 +1,22 @@
+package me.JordanBotfort.bot;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Jordan extends ListenerAdapter
+{
+    public static void main(String[] args) throws LoginException
+    {
+        JDABuilder bot = JDABuilder.createDefault("");
+        bot.addEventListeners(new Commands());
+        bot.enableIntents(GatewayIntent.DIRECT_MESSAGES);
+        bot.build();
+        
+
+    }
+}
+
